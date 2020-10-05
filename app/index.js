9c59c18abe507795a50afd35b94824877ea244e5
Umur Alpay <umuralpay@outlook.com>
2020-10-05 20:32:12 +0300

    health check

--- app/index.js
+++ app/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const health = require("@cloudnative/health-connect");
 const app = express();
 const port  = process.env.PORT || 3000;
+
+const healthCheck = new health.HealthChecker();
+const pingCheck = new health.PingCheck("example.com");
+healthCheck.registerLivenessCheck(pingCheck);
+healthCheck.registerReadinessCheck(pingCheck);
+app.use('/live', health.LivenessEndpoint(healthCheck));
+app.use('/ready', health.ReadinessEndpoint(healthCheck));
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
